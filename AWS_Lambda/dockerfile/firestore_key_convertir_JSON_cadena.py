ae52ca4da1b7229c29d447e12896660106e64337
PQSolutions <pqsolutionsperu@gmail.com>
2024-05-03 23:33:11 -0500

    AWSLambda eliminada

--- AWS_Lambda/dockerfile/firestore_key_convertir_JSON_cadena.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Asegúrate de especificar la ruta correcta al archivo de credenciales
-with open('P:\\PPP\\PQSolutions\\APP\KEYS APP\\fir-hdd-monitor-firebase-adminsdk-l8ui2-f517870b2b.json', 'r') as file:
-    credentials_dict = json.load(file)
-
-credentials_string = json.dumps(credentials_dict)
-print(credentials_string)
